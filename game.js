f40dd796cb56ed0be86c09bf1b996c97fcd000f3
Heraclio <ericworknode@gmail.com>
2015-01-25 00:53:58 -0700

    Tile Generation created and asset modification.

--- game.js
+++ game.js
@@ -54,6 +54,7 @@ function start() {
                 update: function () {}
             });
 
+            /*
             testSprite.scale = {x: 2, y: 2};
             //testSprite.filters = [new PIXI.PixelateFilter()];
             app.currentScene.addEntity({
@@ -61,6 +62,8 @@ function start() {
               sprite: testSprite,
               update: function () {}
             });
+            */
+            MapGeneration();
         }, app);
 
         app.addScene(loadingScene);
@@ -74,22 +77,66 @@ function start() {
 
 function MapGeneration() {
     var worldSize = 1024;
-    var tileOffset = 32;
+
     var mapContainer = new PIXI.DisplayObjectContainer();
     var texture = new PIXI.RenderTexture();
     texture.render(mapContainer);
 
-    /*
-        var map = [
-        for(){
-            for(){
-                [5,5,5,5,5],
-            }
-            if(i-n eq 1){
-                [5,5,5,5,5]];
-            }
+    function onloadGenerate(x,oX,oY){
+        var label = x.split('.');
+        var methodLabel = 'load'+label[0];
+        methodLabel = new PIXI.Sprite.fromFrame(x);
+        methodLabel.scale = {x: 2, y: 2};
+        app.currentScene.addEntity({
+          type: label[0],
+          sprite: methodLabel,
+          update: function () {}
+        });       
+        // var assign obj and add it to the scene and //
+        methodLabel.position = new SL.Vec2(oX, oY);
+    }
+//DRAW//
+//INVOKE WHILE < World Map Size//
+    var jsonObj;
+    xhr = new XMLHttpRequest();
+    xhr.onload = function () { 
+        jsonObj = JSON.parse(this.responseText); 
+        var keys = [];
+        var tileKeys = [];
+        var oX=0;
+        var oY=0;
+        var i = 0;
+        for(var key in jsonObj.frames){
+          keys.push(key);
         }
-    */
+        for(var i=0;i<keys.length;i++){
+            if(keys[i] == 'alert.PNG' || keys[i] == 'clickGround1.PNG' || keys[i] == 'clickGround2.PNG' || keys[i] == 'clickGround3.PNG' || keys[i] == 'puppyWalk1.PNG' || keys[i] == 'puppyWalk2.PNG' || keys[i] == 'puppyWalk3.PNG'){
+            }else{
+                tileKeys.push(keys[i]);
+            }            
+        }
+        for(var j=0;j<worldSize;j+=64){
+            var selector = Math.floor(Math.random() * tileKeys.length);
+            onloadGenerate(tileKeys[selector],oX,oY);
+            if(tileKeys[selector] == 'tree.PNG' || tileKeys[selector] == 'dirt.PNG'){
+                oX += 64;
+            } else {
+                for(var i=0;i<2;i++){
+                    var r = Math.floor(Math.random() * tileKeys.length);
+                    if(tileKeys[r] == 'tree.PNG' || tileKeys[r] == 'dirt.PNG'){
+                    } else{
+                        var yO = oY;
+                        yO = oY + 64;                      
+                        onloadGenerate(tileKeys[r],oX,yO);
+                    }
+                }                
+                oX = oX;
+            }
+            oX+=64;                            
+        }
+    };
+    xhr.open('get', 'res/atlas.json', true);
+    xhr.send();
     /*
     var TileCheck : function(){
         for(var i=0;i<worldSize;i++){
